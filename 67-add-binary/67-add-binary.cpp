fb76c44ad7e9673dae1d2dadee8d97b13c6e4450
Upasana1216 <222upasanasingh@gmail.com>
2022-01-10 08:31:08 +0530

    Time: 5 ms (17.36%), Space: 6.3 MB (84.88%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -2,25 +2,30 @@ class Solution {
 public:
     string addBinary(string a, string b) {
         
-        int i=a.size()-1;
+        //Basic Idea is we will start adding from last similar to normal addition
+        
+        int i=a.size()-1; 
         int j=b.size()-1;
         
-        string ans="";
-        int c=0;
+        string ans=""; //To maintain the answer
+        int c=0;       //To store the carry
+        
         while(i>=0 || j>=0 || c>0){
+            
             if (i >= 0) {
-                c += a[i] - '0';
+                c += a[i] - '0'; //calculating the carry at a[i]
                 i -= 1;
             }
             if (j >= 0) {
-                c += b[j] - '0';
+                c += b[j] - '0';  //calculating the carry at a[i]
                 j -= 1;
             }
-            ans += (c % 2) + '0';
-            c /= 2;
+            ans += (c % 2) + '0'; //c values can be 0,1,2 so we will get the value to be appended by c%2 as 
+            c /= 2; // carry will be only there if c=2, so calculating carry
         }
         
-        reverse(ans.begin(),ans.end());
-        return ans;
+        reverse(ans.begin(),ans.end()); //since started from last now rreversig
+        
+        return ans; 
     }
 };
