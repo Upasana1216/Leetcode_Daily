af49bafeca2cfad00a73f8235e8bcd0f4c0ec85b
Upasana1216 <222upasanasingh@gmail.com>
2022-01-10 08:39:02 +0530

    Time: 6 ms (6.50%), Space: 5.8 MB (81.00%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int comp = 1;
+        while (comp < n) 
+            comp = (comp << 1) | 1;
+        return n ^ comp;
+    }
+};
