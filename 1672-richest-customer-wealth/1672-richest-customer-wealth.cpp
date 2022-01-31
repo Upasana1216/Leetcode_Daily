070d563cf2f53b971c3c1c702a52d835d7abb9b3
Upasana1216 <222upasanasingh@gmail.com>
2022-01-31 10:21:58 +0530

    Time: 4 ms (90.57%), Space: 7.9 MB (13.11%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        
+        int ans=0;
+        for(int i=0;i<accounts.size();i++){
+            int s=0;
+            for(int j=0;j<accounts[i].size();j++){
+                s+=accounts[i][j];
+            }
+            ans=max(ans,s);
+        }
+        
+        return ans;
+    }
+};
