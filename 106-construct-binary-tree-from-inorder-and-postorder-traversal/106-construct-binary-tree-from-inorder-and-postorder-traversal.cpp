690cb2eda2638bcbe8da0462f0a499570a381ef0
Upasana1216 <222upasanasingh@gmail.com>
2022-03-03 07:35:56 +0530

    Time: 35 ms (43.32%), Space: 26.3 MB (57.29%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,29 +11,29 @@
  */
 class Solution {
 public:
-     TreeNode * constructTree(vector < int > & preorder, int preStart, int preEnd, vector 
+     TreeNode * constructTree(vector < int > & postorder, int postStart, int postEnd, vector 
  < int > & inorder, int inStart, int inEnd, map < int, int > & mp) {
-      if (preStart > preEnd || inStart > inEnd) return NULL;
+      if (postStart > postEnd || inStart > inEnd) return NULL;
 
-      TreeNode * root = new TreeNode(preorder[preEnd]);
+      TreeNode * root = new TreeNode(postorder[postEnd]);
       int elem = mp[root -> val];
       int nElem = elem-inStart;
 
-      root -> left = constructTree(preorder, preStart, preStart + nElem -1, inorder,
+      root -> left = constructTree(postorder, postStart, postStart + nElem -1, inorder,
       inStart, elem - 1, mp);
-      root -> right = constructTree(preorder, preStart + nElem , preEnd -1, inorder, 
+      root -> right = constructTree(postorder, postStart + nElem , postEnd -1, inorder, 
       elem + 1, inEnd, mp);
 
       return root;
 }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int preStart = 0, preEnd = postorder.size() - 1;
+        int postStart = 0, postEnd = postorder.size() - 1;
           int inStart = 0, inEnd = inorder.size() - 1;
 
           map < int, int > mp;
           for (int i = inStart; i <= inEnd; i++) {
             mp[inorder[i]] = i;
           }
-          return constructTree(postorder, preStart, preEnd, inorder, inStart, inEnd, mp);
+          return constructTree(postorder, postStart, postEnd, inorder, inStart, inEnd, mp);
     }
 };
